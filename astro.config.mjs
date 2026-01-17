8dec93b9920d8e42d5dfce2548165895db27db8c
oc-2 <185859974+oc-2@users.noreply.github.com>
2026-01-17 02:37:40 +0000

    Added mdx, svelte, and netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import mdx from '@astrojs/mdx';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), mdx()],
+  adapter: netlify()
+});
